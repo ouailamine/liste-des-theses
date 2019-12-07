f57e1055fddbfb271f9b37fa182c7703a5d035c0
ouailamine <ouailamin84@gmail.com>
2019-12-07 21:40:20 +0100

    premier commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use App\Entity\These;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="main")
+     */
+    public function index()
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/theses", name="thes")
+     * @Method ({"GET"})
+     */
+    public function these()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $thesess = $this->getDoctrine()->getRepository(These::class)->findAll();
+        return $this->render('main/listedestheses.html.twig', array('thesess' => $thesess));
+            
+    }
+
+
+    
+}
---
+++ src/Entity/These.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TheseRepository")
+ */
+class These
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $categorie;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contact;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ecole;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sweb;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(string $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getContact(): ?string
+    {
+        return $this->contact;
+    }
+
+    public function setContact(string $contact): self
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+
+    public function getEcole(): ?string
+    {
+        return $this->ecole;
+    }
+
+    public function setEcole(string $ecole): self
+    {
+        $this->ecole = $ecole;
+
+        return $this;
+    }
+
+    public function getSweb(): ?string
+    {
+        return $this->sweb;
+    }
+
+    public function setSweb(string $sweb): self
+    {
+        $this->sweb = $sweb;
+
+        return $this;
+    }
+}
